2e997709a0a24a0bdd4780912524ecd46fae9579
Alexandros Dorodoulis <aldor94@gmail.com>
2020-04-14 15:36:29 +0200

    refactor: Use uint for cor

--- types/types.go
+++ types/types.go
@@ -2,14 +2,14 @@ package types
 
 type SimpleMap map[byte]string
 
-type CoordinatesLookup map[[2]int]struct{}
+type CoordinatesLookup map[[2]uint]struct{}
 
 type IntrestingCoordinates map[byte]CoordinatesLookup
 type LevelInfo struct {
 	LevelInfo        map[string]string
 	AgentColor       SimpleMap
 	BoxColor         SimpleMap
-	WallsCoordinates map[[2]int]struct{}
+	WallsCoordinates map[[2]uint]struct{}
 	AgentCoordinates IntrestingCoordinates
 	BoxCoordinates   IntrestingCoordinates
 	GoalCoordinates  IntrestingCoordinates
@@ -20,9 +20,9 @@ func GetLevelInfo() LevelInfo {
 		LevelInfo:        make(map[string]string, 2),
 		AgentColor:       SimpleMap{},
 		BoxColor:         SimpleMap{},
-		WallsCoordinates: make(map[[2]int]struct{}, 15),
+		WallsCoordinates: make(map[[2]uint]struct{}, 15),
+		GoalCoordinates:  IntrestingCoordinates{},
 		AgentCoordinates: IntrestingCoordinates{},
 		BoxCoordinates:   IntrestingCoordinates{},
-		GoalCoordinates:  IntrestingCoordinates{},
 	}
 }
