4097830ac26eec2dfdc6ac2a86d220a63f371b22
Alexandros Dorodoulis <aldor94@gmail.com>
2020-05-21 15:38:22 +0200

    feat(ai): Add the ability to create cpu profiles

--- main.go
+++ main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"log"
 	"os"
 	"os/signal"
+	"runtime/pprof"
 	"syscall"
 
 	"github.com/alexdor/dtu-ai-mas-final-assignment/ai"
@@ -11,15 +14,33 @@ import (
 	"github.com/alexdor/dtu-ai-mas-final-assignment/parser"
 )
 
+var cpuprofile = flag.String("cpuprofile", "", "write cpu profile to file")
+
 func main() {
+	flag.Parse()
+	if *cpuprofile != "" {
+		f, err := os.Create(*cpuprofile)
+		if err != nil {
+			log.Fatal(err)
+		}
+		pprof.StartCPUProfile(f)
+		defer pprof.StopCPUProfile()
+	}
+
 	sigs := make(chan os.Signal, 1)
 	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
 
 	go func() {
 		<-sigs
 		fmt.Println("Got a timeout")
+
+		if *cpuprofile != "" {
+			pprof.StopCPUProfile()
+		}
+
 		os.Exit(1)
 	}()
+
 	communication.Init()
 
 	levelInfo, currentState, err := parser.ParseLevel()
